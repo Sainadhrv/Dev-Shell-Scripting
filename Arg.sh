71f08fd104642d46fe4fbe3e5dbf00745ebf9459
sainadh <sainadhrv1619@gmail.com>
2025-03-05 22:03:05 -0500

    My bujji bangaram tho

---
+++ Arg.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+VARIABLE1=$1
+VARIABLE2=$2
+echo "$VARIABLE1::$VARIABLE2 i love yo so much nani ga"
+echo "$VARIABLE2::$VARIABLE1 Nenu cheppanu pora waste fellow"
+echo "$VARIABLE1::$VARIABLE2 Cheppaku kani na bujji kanna anava"
+echo "$VARIABLE2::$VARIABLE1 Adhi naku chala special only naku nachinapude anta"
+echo "$VARIABLE1::$VARIABLE2 Your wish princess"
+echo "$VARIABLE2::$VARIABLE1 Naku nuvvu ne istam ante nachadu&Konchem telugu lo cheppandi sir"
+echo "$VARIABLE1::$VARIABLE2 Oyy vurike annane ummmmmmmmmmmmmmah"
