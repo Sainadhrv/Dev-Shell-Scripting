0295621f3e11ab8997bb9c4eca9f5ba1e3e4b4fb
sainadh <sainadhrv1619@gmail.com>
2025-03-05 21:47:06 -0500

    new changes to file1

--- Practice1.sh
+++ Practice1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NAME1:SAINADH
-NAME2:RAMESH
-echo $NAME1:"Hi ramesh,how are you?"
-echo $NAME2:"Hello Sainadh,I am doing good."
+NAME1=SAINADH
+NAME2=RAMESH
+echo $NAME1:"Hi$NAME2 ,how are you?"
+echo $NAME2:"Hello $NAME1,I am doing good."
