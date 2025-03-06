0da5d089152b5b686f728d2dd5a2c58f419c549b
sainadh <sainadhrv1619@gmail.com>
2025-03-06 16:15:58 -0500

    new sum value

---
+++ Condition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "all variables passed $@"
+echo  "number of  variables $#"
+echo "current script using $O"
+echo "present  directory $PWD"
+echo "home diectory of current user $HOME"
+echo "which user running this script $USER" 
+echo "process id of current script $$"
+sleep 30 &
+echo "Process id of last command $!"
